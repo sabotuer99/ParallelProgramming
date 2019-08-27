8b3151e801cb9e04cc105118253b9aabd5648726
troy <sabotuer99@gmail.com>
2019-08-26 22:52:09 -0700

    finished example assignment

--- week1/example/src/main/scala/example/Lists.scala
+++ week1/example/src/main/scala/example/Lists.scala
@@ -23,7 +23,7 @@ object Lists {
    * @param xs A list of natural numbers
    * @return The sum of all elements in `xs`
    */
-    def sum(xs: List[Int]): Int = ???
+    def sum(xs: List[Int]): Int = xs.foldRight(0)(_ + _)
   
   /**
    * This method returns the largest element in a list of integers. If the
@@ -38,5 +38,8 @@ object Lists {
    * @return The largest element in `xs`
    * @throws java.util.NoSuchElementException if `xs` is an empty list
    */
-    def max(xs: List[Int]): Int = ???
+    def max(xs: List[Int]): Int = xs match {
+      case Nil => throw new java.util.NoSuchElementException("no max of empty list")
+      case _ => xs.foldRight (xs (0) ) (math.max (_, _) )
+    }
   }
